6928aa4e130720e21a386b590de58c6c5de8f76e
damiaanh <d.houtschild@gmail.com>
2021-05-18 16:56:15 +0200

    added cleanlist function to model

--- model.py
+++ model.py
@@ -1,8 +1,30 @@
+import string
+
 class BigramModel:
 
     def __init__(self, tokens):
-        pass
-    
+        self.tokens = tokens
+        self.freqtable = {}
+        self.cleanlist = self.clean_tokens()
+
+    def clean_tokens(self):
+        startmark = "<s>"
+        endmark = "</s>"
+        cleanlist = []
+        for sentence in tokens:
+            cleanlist.append(startmark)
+            for token in sentence:
+                token.strip(string.punctuation)
+                token.lower()
+                if token is not "":     # Only continue for tokens that are not empty (due to remove of punctuation)
+                    if token in self.freqtable:  # Count frequencies while looping over the text
+                        self.freqtable[token] += 1
+                    else:
+                        self.freqtable[token] = 1
+                    cleanlist.append(token)
+            cleanlist.append(endmark)
+        return cleanlist
+
     def p_raw(self, w, w_n):
         pass
 
